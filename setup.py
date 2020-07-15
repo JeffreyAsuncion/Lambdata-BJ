dacc1b5e167d7c15444e92296ab0bfc7c36b91dd
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-07-14 21:18:40 -0400

    minor formatting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_bjja",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Jeffrey Asuncion",
     author_email="jeffrey.l.asuncion@gmail.com",
     description="A small example package",
